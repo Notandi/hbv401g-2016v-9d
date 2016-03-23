f044a565e0df9ef187ba8175249de888acebb967
Óskar <Notandi@users.noreply.github.com>
2016-03-23 15:12:55 +0000

    Car og Trip updateað

--- dayTrip/Car.java
+++ dayTrip/Car.java
@@ -1,5 +1,12 @@
 package dayTrip;
 
 public class Car {
-
+	private String Type;
+	private String Description;
+	private	String Picture;
+	private int Price;
+	private String Keywords;
+	public void changePrices(int price){
+		this.Price = price;
+	};
 }
--- dayTrip/Trip.java
+++ dayTrip/Trip.java
@@ -1,5 +1,27 @@
 package dayTrip;
 
 public class Trip {
-
+	private int  Popularity;
+	private String Location;
+	private int Difficulty;
+	private String Description;
+	private int Price;
+	private String Transportation;
+	private String[] Reviews;
+	private int[] Ratings;
+	private String Picture;
+	private Attraction[] Attraction;
+	private String Keywords;
+	public void addReview(String review){
+		
+	};
+	public void changePopularity(int difference){
+		
+	};
+	public void changePrice(int price){
+		
+	};
+	public void rate(int rating){
+		
+	};
 }
