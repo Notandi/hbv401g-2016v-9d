9e334426af254e5266493f3d3f71d79211801dca
Óskar <Notandi@users.noreply.github.com>
2016-03-23 15:24:35 +0000

    Bætti við í Attraction

--- dayTrip/Attraction.java
+++ dayTrip/Attraction.java
@@ -1,5 +1,14 @@
 package dayTrip;
 
 public class Attraction {
-
+	private String type;
+	private String location;
+	private String description;
+	private String picture;
+	public void changePicture(String picture){
+		this.picture = picture;
+	};
+	public void changeDescription(String description){
+		this.description = description;
+	};
 }
