d72b5c98e8af02cbf311bb33cf106ebae8b97a98
dap8 <dap8@hi.is>
2016-03-23 15:58:33 +0000

    kláraði manager abstract klasa

--- dayTrip/Manager.java
+++ dayTrip/Manager.java
@@ -6,8 +6,7 @@ public abstract class Manager<T> {
 	private T[] ManagedObjects;
 	
 	public abstract <T> T[] search(Query query);
-	
-	
-	
+	public abstract <T> void addObject(T t);
+	public abstract <T> void removeObject(T t);
 
 }
