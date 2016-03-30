b6284e1c1ec7acdbb0c83a8bbda47e8441d61aa0
Óskar <Notandi@users.noreply.github.com>
2016-03-30 13:18:43 +0000

    Bætti við get og set í Attraction

--- dayTrip/Attraction.java
+++ dayTrip/Attraction.java
@@ -9,19 +9,42 @@ public class Attraction {
 	
 	public Attraction(String type, String location, String description, String picture) {
 		
+		this.setType(type);
+		this.setLocation(location);
+		this.setDescription(description);
+		this.setPicture(picture);
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
 		this.type = type;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
 		this.location = location;
-		this.description = description;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
 		this.picture = picture;
 	}
-	
-	public void changePicture(String picture) {
-		
-		this.picture = picture;
+
+	public String getDescription() {
+		return description;
 	}
-	
-	public void changeDescription(String description) {
-		
+
+	public void setDescription(String description) {
 		this.description = description;
 	}
+	
 }
