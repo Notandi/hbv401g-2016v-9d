71f36095bf4877f79b34e9fdd90890fd42561722
Óskar <Notandi@users.noreply.github.com>
2016-03-30 16:29:21 +0000

    Lagaði Booking Manager

--- dayTrip/BookingManager.java
+++ dayTrip/BookingManager.java
@@ -2,13 +2,4 @@ package dayTrip;
 
 public class BookingManager extends Manager<Booking> {
 
-	private Booking[] bookings;
-
-	public Booking[] getBookings() {
-		return bookings;
-	}
-
-	public void setBookings(Booking[] bookings) {
-		this.bookings = bookings;
-	}
 }
