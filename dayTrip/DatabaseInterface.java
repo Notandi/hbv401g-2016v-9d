efaad4bc450ebe3b1947a71e710cc0db915d4656
Óskar <Notandi@users.noreply.github.com>
2016-04-01 14:32:00 +0000

    DatabaseInterface.java notar MockData.java

--- dayTrip/DatabaseInterface.java
+++ dayTrip/DatabaseInterface.java
@@ -6,8 +6,20 @@ public class DatabaseInterface {
 	
 	public ArrayList<?> select(Query query)
 	{
-		ArrayList<String> x = new ArrayList<String>();
-		return x;
+		int manager = query.getType();
+		switch(manager){
+		case 1: ArrayList<Car> x1 = MockData.MockCar(query);
+				return x1;
+		case 2:	ArrayList<Attraction> z1 =  MockData.MockAttraction(query);
+				return z1;
+		case 3: ArrayList<Trip> q1 = MockData.MockTrip(query);
+				return q1;
+		case 4:	ArrayList<Booking> r1 = MockData.MockBooking(query);
+				return r1;
+		case 5: ArrayList<Customer> s1 =  MockData.MockCustomer(query);
+				return s1;
+		}
+		return null;
 	}
 	
 	public int insert(Pair[] query)
--- dayTrip/MockData.java
+++ dayTrip/MockData.java
@@ -1,5 +1,37 @@
 package dayTrip;
 
-public class MockData {
+import java.util.ArrayList;
 
+public class MockData {
+	
+	public static ArrayList<Car> MockCar(Query query){
+		Car mockcar = new Car(null, null, null, 0, null, false);
+		ArrayList<Car> carArray = new ArrayList<Car>();
+		carArray.add(mockcar);
+		return carArray;
+	}
+	public static ArrayList<Attraction> MockAttraction(Query query){
+		Attraction mockattraction = new Attraction(null, null, null, null);
+		ArrayList<Attraction> attractionArray = new ArrayList<Attraction>();
+		attractionArray.add(mockattraction);
+		return attractionArray;
+	}
+	public static ArrayList<Booking> MockBooking(Query query){
+		Booking mockbooking = new Booking(null, null, null, 0, 0);
+		ArrayList<Booking> bookingArray = new ArrayList<Booking>();
+		bookingArray.add(mockbooking);
+		return bookingArray;
+	}
+	public  static ArrayList<Trip> MockTrip (Query query ){
+		Trip mocktrip = new Trip(0, null, 0, null, 0, null, null, null, null, null, null);
+		ArrayList<Trip> tripArray = new ArrayList<Trip>();
+		tripArray.add(mocktrip);
+		return tripArray; 
+	}
+	public  static ArrayList<Customer> MockCustomer (Query query){
+		Customer mockcustomer = new Customer(null, 0, null, null, null);
+		ArrayList<Customer> customerArray = new ArrayList<Customer>();
+		customerArray.add(mockcustomer);
+		return customerArray;
+	}
 }
