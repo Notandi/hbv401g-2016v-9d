aed7bd693b9ee0cd6e8f1599b8d4e6d7ccafb230
steinar12 <ssa12@hi.is>
2016-04-03 16:56:22 +0000

    Comment í MockData klasanum

--- dayTrip/MockData.java
+++ dayTrip/MockData.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class MockData {
 	
+	// The five following functions create and return mock data of various types.
+	
+	// Returns an ArrayList containing a single Car object.
 	public static ArrayList<Car> MockCar(Query query){
 		String[] keywords = {"jeppi", "97"};
 		Car mockcar = new Car("Ford 356", "97 model, jeppi, kemst hvert sem er.", "link", 30000, keywords, true);
@@ -11,18 +14,21 @@ public class MockData {
 		carArray.add(mockcar);
 		return carArray;
 	}
+	// Returns an ArrayList containing a single Attraction object.
 	public static ArrayList<Attraction> MockAttraction(Query query){
 		Attraction mockattraction = new Attraction("Natural wonder","Grindavík", "Eðal bær, mjög fallegur.", "link");
 		ArrayList<Attraction> attractionArray = new ArrayList<Attraction>();
 		attractionArray.add(mockattraction);
 		return attractionArray;
 	}
+	// Returns an ArrayList containing a single Booking object.
 	public static ArrayList<Booking> MockBooking(Query query){
 		Booking mockbooking = new Booking("Jonathan Blake jr.", null, null, 14000, 6);
 		ArrayList<Booking> bookingArray = new ArrayList<Booking>();
 		bookingArray.add(mockbooking);
 		return bookingArray;
 	}
+	// Returns an ArrayList containing a single Trip object.
 	public  static ArrayList<Trip> MockTrip (Query query ){
 		String[] keywords = {"Hvalfjörður", "göngutúr"};
 		Trip mocktrip = new Trip(0, "Hvalfjörður", 2, "Göngutúr um fallegt landsvæði.", 1500, "Bus", null, null, "link", null, keywords);
@@ -30,12 +36,16 @@ public class MockData {
 		tripArray.add(mocktrip);
 		return tripArray; 
 	}
+	// Returns an ArrayList containing a single Customer object.
 	public  static ArrayList<Customer> MockCustomer (Query query){
 		Customer mockcustomer = new Customer("Ósk Ólafsdóttir", 15, "Female", "Indian", "iamindian@india.in");
 		ArrayList<Customer> customerArray = new ArrayList<Customer>();
 		customerArray.add(mockcustomer);
 		return customerArray;
 	}
+	
+	// These functions should delete or insert data in the database, 
+	// currently they return the integer 1 if the function call was successful.
 	public static int insert(Pair[] query) {
 		return 1;
 	}
